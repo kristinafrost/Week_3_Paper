33562a08be96b100f6ed9dfef66446605d58e55e
Kristina Frost <kristinafrost@tw-mbp15retina-kfrost.gateway.2wire.net>
2014-06-20 20:33:01 -0700

    Initial Commit

---
+++ Week_3_Paper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Week_3_Paper
+//
+//  Created by Kristina Frost on 6/20/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
